5ab0b74aca74f1949e80075b7c8db2b9d3ff859d
Optical23 <40125489+Optical23@users.noreply.github.com>
2021-11-03 16:31:03 -0500

    added arrays of characters and make an array of the user chosen acceptable ones

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,21 +1,34 @@
 // Assignment code here
-
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numerials = ["0","1","2","3","4","5","6","7","8","9"];
+var special = [" ","!",'"',"#","$","%","&","'","(",")","*","+","-",".","/",":",";","<","=","/",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
 
 function generatePassword(){
   var passWordLength = window.prompt("Select a length between 8 and 128 characters for your password.");
-  if(passWordLength > 8 && passWordLength < 128){
+  if(passWordLength >= 8 && passWordLength <= 128){
     var includeLowerCase = window.confirm("Would you like to include lowercase characters?");
     var includeUpperCase = window.confirm("Would you like to include uppercase characters?");
     var includeNumerials = window.confirm("Would you like to include numeric characters?");
     var includeSpecial = window.confirm("Would you like to include special characters?");
     if(!includeLowerCase && !includeUpperCase && !includeNumerials && !includeSpecial){
-      window.alert("You did not select any character sets to use in the password, Please try again.")
-      return;
+      window.alert("You did not select any character sets to use in the password, Please try again.");
+    }else{
+      return createPassword();
     }
   }else{
     window.alert("You did not select a password length between 8 and 128 characters. Plese try again.");
   }
-
+  function createPassword() {
+    var thePassword = "";
+    var acceptableCharacters = [];
+    var acceptableCharacters = (includeLowerCase) ? acceptableCharacters.concat(lowerCase) : acceptableCharacters;
+    var acceptableCharacters = (includeUpperCase) ? acceptableCharacters.concat(upperCase) : acceptableCharacters;
+    var acceptableCharacters = (includeNumerials) ? acceptableCharacters.concat(numerials) : acceptableCharacters;
+    var acceptableCharacters = (includeSpecial) ? acceptableCharacters.concat(special) : acceptableCharacters;
+    
+    return acceptableCharacters;
+  }
 
 }
 
